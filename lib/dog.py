6700ca5a8bb4ee26e511f6b4d2994b0aa93b20da
Mark Njenga <marknjenga25@gmail.com>
2024-09-07 08:13:46 +0300

    "Added implementation to Dog and Person classes with attribute validation"

--- lib/dog.py
+++ lib/dog.py
@@ -12,4 +12,35 @@ APPROVED_BREEDS = [
 ]
 
 class Dog:
-    pass
+
+
+    APPROVED_BREEDS = ["Pug", "Bulldog", "Poodle", "Chihuahua", "Labrador"]
+
+    def __init__(self, name=None, breed=None):
+
+        if name is not None:
+            self.set_name(name)
+        else:
+            self.name = None
+
+        if breed is not None:
+            self.set_breed(breed)
+        else:
+            self.breed = None
+
+    def set_name(self, name):
+   
+        if not isinstance(name, str) or len(name) < 1 or len(name) > 25:
+            print("Name must be string between 1 and 25 characters.")
+        else:
+            self.name = name
+
+    def set_breed(self, breed):
+     
+        if breed not in self.APPROVED_BREEDS:
+            print("Breed must be in list of approved breeds.")
+        else:
+            self.breed = breed
+
+
+
--- lib/person.py
+++ lib/person.py
@@ -16,4 +16,35 @@ APPROVED_JOBS = [
 ]
 
 class Person:
-    pass
+    APPROVED_JOBS = ["ITC", "Sales"]
+
+    def __init__(self, name=None, job=None):
+
+        if name is not None:
+            self.name = self._validate_name(name)
+        else:
+            self.name = None
+
+        if job is not None:
+            self.job = self._validate_job(job)
+        else:
+            self.job = None
+
+    def _validate_name(self, name):
+
+        if not isinstance(name, str):
+            print("Name must be string between 1 and 25 characters.")
+            return None
+        elif len(name) < 1 or len(name) > 25:
+            print("Name must be string between 1 and 25 characters.")
+            return None
+        else:
+            return name.title()
+
+    def _validate_job(self, job):
+
+        if job not in self.APPROVED_JOBS:
+            print("Job must be in list of approved jobs.")
+            return None
+        else:
+            return job
